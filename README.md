9cb60ebaba528e9aaf1bff457e78ebda546080b5
Taisuke Oono <taisukeoono@TaisukenoMacBook-puro.local>
2020-02-06 14:45:00 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# play-svelte
+
+## dev
+
+```bash
+$ cd path
+$ npm install
+$ npm run dev
+```
---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+  export let name;
+  // image folder is pubic/
+  let image = './images/profile.webp';
+</script>
+
+<main>
+	<h1>{name} portfolio</h1>
+  <img src={image} alt="dd">
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
